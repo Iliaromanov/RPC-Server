f27b211b1e6dd32eaa7959df8d4b98b10ed08b79
Iliaromanov <iliaromanov033@gmail.com>
2021-07-15 19:03:11 -0400

    moving main.go to out of client, setting up and serving RCP microservice

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+)
+
+/*
+RPC rules:
+	- All functions must be methods
+	- Must have two arguments
+		* The first is what the client provides to the API
+	     	and the second is what the API returns to the client
+	- One of the return types must be error
+*/
 
 type Item struct {
 	title string
@@ -13,63 +27,92 @@ var database []Item
 
 // Retrieves an item and its index from database given its title
 //  returns empty Item if not found
-func GetByTitle(title string) (int, Item) {
+func (api *API) GetByTitle(title string, reply *Item) (int, error) {
 	for i, item := range database {
 		if item.title == title {
-			return i, item
+			*reply = item
+			return i, nil
 		}
 	}
-	return -1, Item{}
+	return -1, nil
 }
 
 // Adds a new Item to the database and returns it
-func AddItem(item Item) Item {
+func (api *API) AddItem(item Item, reply *Item) error {
 	database = append(database, item)
-	return item
+	*reply = item
+	return nil
 }
 
 // Updates an Items body and returns the new item
 //  if not found, returns empty item
-func UpdateItem(title string, edit Item) Item {
-	idx, _ := GetByTitle(title)
+func (api *API) UpdateItem(edit Item, reply *Item) error {
+	var old_item Item
+	idx, _ := api.GetByTitle(edit.title, &old_item)
 
 	if idx != -1 {
 		database[idx] = edit
-		return edit
+		*reply = edit
 	}
 
-	return Item{}
+	return nil
 }
 
 // Removes an item from the database
 // **Requires that item exists in database
-func DeleteItem(item Item) Item {
-	idx, _ := GetByTitle(item.title)
+func (api *API) DeleteItem(item Item, reply *Item) error {
+	var old_item Item
+	idx, _ := api.GetByTitle(item.title, &old_item)
 
 	database = append(database[:idx], database[idx+1:] ...)
+	*reply = old_item
 
-	return item
+	return nil
 }
 
 func main() {
-	// Testing
-	fmt.Println("initial database: ", database)
-	a := Item{"one", "this is the first item"}
-	b := Item{"two", "second item here"}
-	c := Item{"three", "third item"}
+	var api = new(API)  // new allocates memory for an empty struct
+	err := rpc.Register((api))
 
-	AddItem(a)
-	AddItem(b)
-	AddItem(c)
-	fmt.Println("second database: ", database)
+	if err != nil {
+		log.Fatal("error registering API", err)
+	}
 
-	DeleteItem(b)
-	fmt.Println("third database: ", database)
+	rpc.HandleHTTP()
 
-	UpdateItem("three", Item{"second", "changed third to second"})
-	fmt.Println("fourth database: ", database)
+	listener, err := net.Listen("tcp", ":5000")
 
-	_, x := GetByTitle("second")
-	_, y := GetByTitle("one")
-	fmt.Println(x, y)
+	if err != nil {
+		log.Fatal("Listener error: ", err)
+	}
+
+	log.Printf("serving rpc on port %d", 5000)
+	err = http.Serve(listener, nil)
+	if err != nil {
+		log.Fatal("error serving: ", err)
+	}
+
+	// // Testing
+	// var api API
+	// fmt.Println("initial database: ", database)
+	// a := Item{"one", "this is the first item"}
+	// b := Item{"two", "second item here"}
+	// c := Item{"three", "third item"}
+
+	// var reply Item
+	// api.AddItem(a, &reply)
+	// api.AddItem(b, &reply)
+	// api.AddItem(c, &reply)
+	// fmt.Println("second database: ", database)
+
+	// api.DeleteItem(b, &reply)
+	// fmt.Println("third database: ", database)
+
+	// api.UpdateItem(Item{"three", "changed third to second"}, &reply)
+	// fmt.Println("fourth database: ", database)
+
+	// var x, y Item
+	// api.GetByTitle("three", &x)
+	// api.GetByTitle("one", &y)
+	// fmt.Println(x, y)
 }
